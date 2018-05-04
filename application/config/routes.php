5da37c7227cbb3c199eeff3dce6526b47b35aea3
Daniel V. Pinacho <daniel.pinacho@gmail.com>
2018-05-04 15:55:50 -0500

    Interfaz del chat

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['inicio'] = 'Inicio_controller';
+$route['chat'] = 'Chat_controller';
+$route['preguntar']['POST'] = 'Conversation/conversation';
---
+++ application/controllers/Chat_controller.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Chat_controller extends CI_Controller {
+	public function index() {
+		$this->load->view('chat/index');
+	}
+}
