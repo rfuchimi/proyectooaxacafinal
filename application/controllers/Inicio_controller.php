6923b5d13cc8fe88c55a470de7be58977fcff1ff
unknown <asisduzi@gmail.com>
2018-05-26 12:50:50 -0500

    Ejemplo amcharts

--- application/controllers/Inicio_controller.php
+++ application/controllers/Inicio_controller.php
@@ -3,9 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Inicio_controller extends CI_Controller {
 	public function index()
-	{		
+	{
 		$this->load->view('layout/header');
 		$this->load->view('inicio');
+		$this->load->view('chart');
 		$this->load->view('layout/footer');
 	}
 }
